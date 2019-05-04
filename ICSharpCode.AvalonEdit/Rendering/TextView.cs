ba5e2d699e6c6d26f0cf0ace179875a4291e2d44
jkuehner <jochen.kuehner@kardex.com>
2019-05-04 11:32:52 +0200

    bugfix #20, fix current line highlightning

--- ICSharpCode.AvalonEdit/Rendering/TextView.cs
+++ ICSharpCode.AvalonEdit/Rendering/TextView.cs
@@ -1235,7 +1235,13 @@ namespace ICSharpCode.AvalonEdit.Rendering
 			DisconnectFromTextView(renderer);
 			InvalidateLayer(renderer.Layer);
 		}
-		
+
+		protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+		{
+			base.OnRenderSizeChanged(sizeInfo);
+			InvalidateLayer(KnownLayer.Selection);
+		}
+
 		/// <inheritdoc/>
 		protected override void OnRender(DrawingContext drawingContext)
 		{
